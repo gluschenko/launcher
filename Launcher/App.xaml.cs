bd666919b113a239789932aa04d572e2c246c8a3
Alexander Gluschenko <qugluschenko@gmail.com>
2019-10-23 19:46:53 +0300

    Change version

--- Launcher/App.xaml.cs
+++ Launcher/App.xaml.cs
@@ -12,7 +12,7 @@ namespace Launcher
     public partial class App : Application
     {
         public const string Title = "Launcher";
-        public const string Version = "0.1";
+        public const string Version = "2019.10";
         public const string ConfigPath = "Launcher.json";
         public const string PrefsPath = "LauncherPrefs.json";
         public const string DownloadsDirectory = "Downloads";
